4426e5cfb88e8a0b3e27bd1ff7df7b9bd839cf04
huongtran182 <92384719+huongtran182@users.noreply.github.com>
2025-05-13 22:42:36 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,13 +88,13 @@ with st.sidebar:
     st.page_link("pages/9_Contact_Us.py", label="Contact Us")
 
 # Main content
-st.title("🧠 CNPD: Crustacean Neuropeptide Database")
+st.Image.open("Assets/Img/CNPD_Banner.png")
 st.markdown("""
 ### WELCOME TO CNPD: THE CRUSTACEAN NEUROPEPTIDE DATABASE
 
 Neuropeptides are critical signaling molecules involved in numerous physiological processes, including metabolism, reproduction, development, and behavior. In crustaceans, neuropeptides regulate key biological functions such as molting, feeding, and immune responses. Despite their significance, crustacean neuropeptides remain underrepresented in existing neuropeptide databases.
 
-To address this gap, we introduce **The Crustacean Neuropeptide Database (CNPD)** – A comprehensive resource for neuropeptide research in crustacean species. CNPD systematically curates experimentally confirmed and predicted neuropeptides from various crustacean species using Peer-reviewed literature, Mass spectrometry-based peptidomics, and Public protein databases.
+To address this gap, we introduce **The Crustacean Neuropeptide Database (CNPD)** – A comprehensive resource for neuropeptide research in crustacean species. CNPD systematically curates experimentally confirmed and predicted neuropeptides from various crustacean species using peer-reviewed literature, mass spectrometry-based peptidomics, and public protein databases.
 
 This database provides detailed annotations to support **Comparative neurobiology**, **Functional studies**, and **Computational peptide discovery**.
 """)
@@ -106,3 +106,17 @@ CNPD is a community-driven initiative! We welcome contributions from researchers
 
 To collaborate, submit data, or provide feedback, please contact Dr. Lingjun Li at **lingjun.li@wise.edu**
 """)
+
+st.markdown("""
+### HOW TO CITE
+If you use CNPD in your research, please cite:
+Crustacean Neuropeptide Database (CNPD): A curated resource for neuropeptide research in crustacean species. [Authors]. [Journal Name], [Year]. DOI: [XXXX].
+
+st.markdown("""
+### Funding
+This work is supported by [Funding Agencies]
+For other tools developed by the Li Lab, visit **www.lilabs.org/resources**
+
+st.markdown("""
+<i>Last update: Jul 2025<i>
+
