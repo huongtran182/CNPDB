dbf252339416af365355c57517c76988fc861e72
huongtran182 <92384719+huongtran182@users.noreply.github.com>
2025-05-14 20:17:34 -0500

    Update 8_FAQ.py

--- pages/8_FAQ.py
+++ pages/8_FAQ.py
@@ -64,7 +64,7 @@ def render_card(faq):
         display: flex;
         flex-direction: column;
         min-height: 400px;
-        min-width: 250px;
+        min-width: 400px;
     ">
       <!-- underline fixed at 50px down -->
       <div style="
