1e97abd16c1ddc9ae70fa32db5cf1a54263477b9
huongtran182 <92384719+huongtran182@users.noreply.github.com>
2025-05-14 20:41:35 -0500

    Update 8_FAQ.py

--- pages/8_FAQ.py
+++ pages/8_FAQ.py
@@ -128,7 +128,7 @@ for i in range(0, len(faqs), 3):
             render_card(faq)
     # after each row (except the last), insert a 40px tall spacer
     if i + 3 < len(faqs):
-        st.markdown("<div style='height:40px'></div>", unsafe_allow_html=True)
+        st.markdown("<div style='height:30px'></div>", unsafe_allow_html=True)
 
 # footers
 st.markdown("""
