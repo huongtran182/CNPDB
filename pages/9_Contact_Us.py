d5ebf32e73239018062d85acc034e3625f061821
huongtran182 <92384719+huongtran182@users.noreply.github.com>
2025-05-14 19:14:49 -0500

    Update 9_Contact_Us.py

--- pages/9_Contact_Us.py
+++ pages/9_Contact_Us.py
@@ -38,12 +38,15 @@ with col2:
 st.markdown("### OPPORTUNITIES")
 st.markdown("""
 <div style="text-align: justify; text-justify: inter-word;">
-  Join our research efforts! We are continuously expanding this database and welcome contributions from students and postdoctoral fellows with fellowships or scholarships.
-
-
-  If you're interested in advancing neuropeptide research, please reach out to Dr. Li at <a href="mailto:lingjun.li@wisc.edu">lingjun.li@wisc.edu</a>.
-
-  If you want to initiate collaborations, support the development of the CNPD, or have any trouble accessing this database, please email Dr. Li at <a href="mailto:lingjun.li@wisc.edu">lingjun.li@wisc.edu</a>.
+  <p style="margin-bottom: 1.5em;">
+    Join our research efforts! We are continuously expanding this database and welcome contributions from students and postdoctoral fellows with fellowships or scholarships.
+  </p>
+  <p style="margin-bottom: 1.5em;">
+    If you’re interested in advancing neuropeptide research, please reach out to Dr. Li at <a href="mailto:lingjun.li@wisc.edu">lingjun.li@wisc.edu</a>.
+  </p>
+  <p style="margin-bottom: 1.5em;">
+    If you want to initiate collaborations, support the development of the CNPD, or have any trouble accessing this database, please email Dr. Li at <a href="mailto:lingjun.li@wisc.edu">lingjun.li@wisc.edu</a>.
+  </p>
 </div>
 """, unsafe_allow_html=True)
 
