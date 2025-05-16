184ab0581b8e3ca82a54b5c1c7d421988fe1ed11
huongtran182 <92384719+huongtran182@users.noreply.github.com>
2025-05-15 21:17:56 -0500

    Update 7_Glossary.py

--- pages/7_Glossary.py
+++ pages/7_Glossary.py
@@ -34,7 +34,7 @@ st.markdown("""
 
 /* Term cell */
 .glossary-term {
-  background-color: #3e1a5e;
+  background-color: #7f6694;
   color: white;
   padding: 10px 20px;
   border-radius: 10px 0 0 10px;
@@ -47,7 +47,7 @@ st.markdown("""
   background-color: white;
   color: #333;
   padding: 10px 10px;
-  border: 3px solid #3e1a5e;
+  border: 3px solid #7f6694;
   border-radius: 0 10px 10px 0;
   display: flex;
   align-items: center;
