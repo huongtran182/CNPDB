956a157b8d4a882067e5acd6b0e47b16cda4a246
huongtran182 <92384719+huongtran182@users.noreply.github.com>
2025-05-22 10:57:45 -0500

    Update 4_Related_Databases.py

--- pages/4_Related_Databases.py
+++ pages/4_Related_Databases.py
@@ -23,7 +23,7 @@ st.markdown("""
 
   /* 2) Wrapper with border and rounded corners */
   .table-wrapper {
-    border: 1px solid #29004c;
+    border: 0.5px solid #29004c;
     border-radius: 10px;
     overflow: hidden;
     margin-top: 0;
