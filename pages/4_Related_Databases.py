3a6c72e6b96c82ff45c533766a5a66defb8e27e8
huongtran182 <92384719+huongtran182@users.noreply.github.com>
2025-05-22 10:55:38 -0500

    Update 4_Related_Databases.py

--- pages/4_Related_Databases.py
+++ pages/4_Related_Databases.py
@@ -36,7 +36,7 @@ st.markdown("""
     width: 100%;
     border-collapse: collapse;     
     margin-bottom: 0 !important;
-    border: 2px solid #29004c;
+    border: none;
   }
 
   /* 4) Cell padding */
