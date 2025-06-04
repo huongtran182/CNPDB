c02f460bfda36d0a43b609a9e36fb7be52720d5a
huongtran182 <92384719+huongtran182@users.noreply.github.com>
2025-06-03 20:51:51 -0500

    Update 9_Contact_Us.py

--- pages/9_Contact_Us.py
+++ pages/9_Contact_Us.py
@@ -147,7 +147,7 @@ for row_index in range(num_rows):
                     display: flex;
                     flex-direction: column;
                     justify-content: space-between;
-                    min-height: 505px;
+                    min-height: 500px;
                 ">
                   <div class="resource-item">
                     <div class="toc-container">
@@ -166,7 +166,7 @@ for row_index in range(num_rows):
                   ">
                     <h3 style="
                         color: #29004c;
-                        margin: 0;
+                        margin: 5px 0 0 0;
                         text-align: center;
                         font-size: 1em;
                         line-height: 1.2;
