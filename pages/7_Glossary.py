cb1d173312fe257cc4eb2ff41f86b831d7be9f25
huongtran182 <92384719+huongtran182@users.noreply.github.com>
2025-06-04 08:35:04 -0500

    Update 7_Glossary.py

--- pages/7_Glossary.py
+++ pages/7_Glossary.py
@@ -34,7 +34,7 @@ st.markdown("""
 
 /* Term cell */
 .glossary-term {
-  background-color: #543370;
+  background-color: #9e9ac8;
   color: white;
   padding: 10px 20px;
   border-radius: 10px 0 0 10px;
@@ -47,7 +47,7 @@ st.markdown("""
   background-color: white;
   color: #333;
   padding: 10px 10px;
-  border: 3px solid #543370;
+  border: 3px solid #9e9ac8;
   border-radius: 0 10px 10px 0;
   display: flex;
   align-items: center;
