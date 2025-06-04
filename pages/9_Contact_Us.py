88766f48d4a9c3f19e3444bb67826a16b967c75b
huongtran182 <92384719+huongtran182@users.noreply.github.com>
2025-06-04 07:54:10 -0500

    Update 9_Contact_Us.py

--- pages/9_Contact_Us.py
+++ pages/9_Contact_Us.py
@@ -221,14 +221,14 @@ papers = [
         "img": "Assets/Publication_TOC/LobsterDevelopmental.jpeg",
         "title": "Relative Quantitation of Neuropeptides at Multiple Developmental Stages of the American Lobster Using N,N-Dimethyl Leucine Isobaric Tandem Mass Tags",
         "summary": (
-            "With reduced cost and higher labeling efficiency, we employed custom-developed N,N-dimethyl leucine (DiLeu) 4-plex isobaric tandem mass tags as "
-            "an attractive alternative for the relative quantitation of neuropeptides in brain tissue of American lobster Homarus americanus at multiple developmental stages."
+            "With reduced cost and higher labeling efficiency, we employed custom-developed N,N-dimethyl leucine (DiLeu) 4-plex isobaric tandem mass tags "
+            "for the relative quantitation of neuropeptides in brain tissue of American lobster Homarus americanus at multiple developmental stages."
         ),
         "link": "https://pubs.acs.org/doi/full/10.1021/acschemneuro.7b00521"
     },
     {
         "img": "Assets/Publication_TOC/FeedingRat.jpg",
-        "title": "Quantitative MS Reveals Food Intake-Induced Neuropeptide Level Changes in Rat Brain: Functional Assessment of Selected Neuropeptides as Feeding Regulators",
+        "title": "Quantitative MS Reveals Food Intake-Induced Neuropeptide Level Changes in Rat Brain: Functional Assessment of Neuropeptides as Feeding Regulators",
         "summary": (
             "To directly monitor neuropeptidomic changes in response to food intake in the rat nucleus accumbens (NAc), and other key brain regions, we employed a combination "
             "of cryostat dissection, heat stabilization, neuropeptide extraction and label-free quantitative neuropeptidomics via LC- platform."
