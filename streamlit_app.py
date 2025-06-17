7f468de71006ce3364b23c8c0cb6830b175a61a8
huongtran182 <92384719+huongtran182@users.noreply.github.com>
2025-06-17 07:15:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,9 +63,11 @@ Every neuropeptide entry undergoes manual examination to ensure accuracy and rel
 st.markdown("""
 ### GET INVOLVED & CONTRIBUTE
 
-cNPDB is a community-driven initiative! We welcome contributions from researchers in the field of peptidomics, neurobiology, and comparative physiology. Here are some ways to contribute:
-- Submit new neuropeptide sequences
-- Report missing or updated annotations
+cNPDB is a community-driven initiative! We welcome contributions from researchers in the field of peptidomics, neurobiology, and comparative physiology.
+If you’d like to contribute, please visit the "Contact Us" page and fill out the Feedback Form. Here are  a few ways you can get involved:
+- Submit newly discovered neuropeptide sequences
+- Report missing data or suggest data corrections
+- Provide references or annotations
 - Share mass spectrometry datasets
 - Suggest additional features or improvements
 """)
