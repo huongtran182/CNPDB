03df08b7684869697c97f9d857470a6ff8b2652c
huongtran182 <92384719+huongtran182@users.noreply.github.com>
2025-06-19 17:59:28 -0500

    Update 6_Glossary.py

--- pages/6_Glossary.py
+++ pages/6_Glossary.py
@@ -102,7 +102,7 @@ tools = [
     ("Instability Index",     "Predicts whether a peptide is likely to be stable (< 40) or unstable (> 40) <i>in vitro</i>."),
     ("Isoelectric Point (pI)","The pH at which the peptide has no net electrical charge."),
     ("Net Charge pH 7.0",     "The overall positive or negative charge the peptide carries at neutral pH."),
-    ("Aliphatic Index",       "Measures the volume of aliphatic side chains (A, V, I, L), which reflects thermal stability; higher value = more stable at higher temperatures"),
+    ("Aliphatic Index",       "Measures the volume of aliphatic side chains (A, V, I, L), which reflects thermal stability; higher value = more stable at higher temperatures."),
     ("Boman Index",           "Estimates a peptide's potential to bind to proteins; higher values suggest stronger or broader binding."),
     ("Peptide Sequence Alignment",    "Compares two peptide sequences to identify regions of similarity that may indicate functional, structural, or evolutionary relationships."),
     ("Type",                  "Global alignment aligns sequences end-to-end, while Local alignment finds the most similar subregions within the sequences."),
