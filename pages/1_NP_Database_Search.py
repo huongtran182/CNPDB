f9a9287a4fc39835cc41ec9d750a439376df7f7a
huongtran182 <92384719+huongtran182@users.noreply.github.com>
2025-06-20 17:08:58 -0500

    Update 1_NP_Database_Search.py

--- pages/1_NP_Database_Search.py
+++ pages/1_NP_Database_Search.py
@@ -308,7 +308,7 @@ def extract_unique_values(series):
     ))
 
 # Create two columns with a 20px gap using Streamlit's built-in layout
-col_filter, col_main = st.columns([1, 4], gap= "medium")
+col_filter, col_main = st.columns([1, 3], gap= "large")
 
 # Custom container with manual gap
 st.markdown('<div class="custom-col-container">', unsafe_allow_html=True)
@@ -365,40 +365,43 @@ with col_main:
     st.markdown("</div>", unsafe_allow_html=True)
 
     # Family
-    st.markdown('<div class="section-title" style="margin-top: -10px; margin-bottom: 16px;">Family</div>', unsafe_allow_html=True)
+    st.markdown('<div class="section-title" style="margin-top: -10px; margin-bottom: 8px;">Family</div>', unsafe_allow_html=True)
     family_opts = sorted(df['Family'].dropna().unique())
     family_selected = st.multiselect(label=" ", options=family_opts, key="fam", label_visibility="collapsed")
 
+    # Add vertical space here
+    st.markdown("<br><br>", unsafe_allow_html=True)
+
     # Organisms
-    st.markdown('<div class="section-title" style="margin-top: 0px; margin-bottom: 16px">Organisms</div>', unsafe_allow_html=True)
+    st.markdown('<div class="section-title" style="margin-top: 0px; margin-bottom: 8px">Organisms</div>', unsafe_allow_html=True)
     org_opts = extract_unique_values(df['OS'])
     organisms_selected = st.multiselect(label=" ", options=org_opts, key="org", label_visibility="collapsed")
 
     # Tissue
-    st.markdown('<div class="section-title" style="margin-top: 0px; margin-bottom: 16px">Tissue</div>', unsafe_allow_html=True)
+    st.markdown('<div class="section-title" style="margin-top: 0px; margin-bottom: 8px">Tissue</div>', unsafe_allow_html=True)
     tissue_opts = extract_unique_values(df['Tissue'])
     tissue_selected = st.multiselect(label=" ", options=tissue_opts, key="tissue", label_visibility="collapsed")
 
     # PTM
-    st.markdown('<div class="section-title" style="margin-top: 0px; margin-bottom: 16px">Post-translational modifications (PTM)</div>', unsafe_allow_html=True)
+    st.markdown('<div class="section-title" style="margin-top: 0px; margin-bottom: 8px">Post-translational modifications (PTM)</div>', unsafe_allow_html=True)
     ptm_opts = extract_unique_values(df['PTM'])
     ptm_selected = st.multiselect(label=" ", options=ptm_opts, key="ptm", label_visibility="collapsed")
 
     # Existence
-    st.markdown('<div class="section-title" style="margin-top: 0px; margin-bottom:16px">Existence</div>', unsafe_allow_html=True)
+    st.markdown('<div class="section-title" style="margin-top: 0px; margin-bottom: 8px">Existence</div>', unsafe_allow_html=True)
     exist_opts      = extract_unique_values(df['Existence'])
     existence_selected = st.multiselect(label=" ", options=exist_opts, key="exist", label_visibility="collapsed")
 
      # --- NEW: Filters for Sheet 2 data ---
-    st.markdown('<div class="section-title" style="margin-top: 0px; margin-bottom: 16px">Physiological Studies or Applications</div>', unsafe_allow_html=True)
+    st.markdown('<div class="section-title" style="margin-top: 0px; margin-bottom: 8px">Physiological Studies or Applications</div>', unsafe_allow_html=True)
     topic_opts = extract_unique_values(df['Topic'])
     topic_selected = st.multiselect(label=" ", options=topic_opts, key="topic", label_visibility="collapsed")
 
-    st.markdown('<div class="section-title" style="margin-top: 0px; margin-bottom: 16px">Instrument</div>', unsafe_allow_html=True)
+    st.markdown('<div class="section-title" style="margin-top: 0px; margin-bottom: 8px">Instrument</div>', unsafe_allow_html=True)
     instrument_opts = extract_unique_values(df['Instrument'])
     instrument_selected = st.multiselect(label=" ", options=instrument_opts, key="instrument", label_visibility="collapsed")
     
-    st.markdown('<div class="section-title" style="margin-top: 0px; margin-bottom: 16px">Technique</div>', unsafe_allow_html=True)
+    st.markdown('<div class="section-title" style="margin-top: 0px; margin-bottom: 8px">Technique</div>', unsafe_allow_html=True)
     technique_opts = extract_unique_values(df['Technique'])
     technique_selected = st.multiselect(label=" ", options=technique_opts, key="technique", label_visibility="collapsed")
 
