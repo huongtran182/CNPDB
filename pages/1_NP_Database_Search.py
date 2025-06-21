7ef51c8528e69e25b16ea61c56a081a2cb584512
huongtran182 <92384719+huongtran182@users.noreply.github.com>
2025-06-20 21:48:38 -0500

    Update 1_NP_Database_Search.py

--- pages/1_NP_Database_Search.py
+++ pages/1_NP_Database_Search.py
@@ -291,7 +291,7 @@ st.markdown(
 st.markdown('<div class="main-search-container">', unsafe_allow_html=True)
 
 #--- Load Data ---
-df = pd.read_csv("Assets/20250617_cNPDB.csv")
+df = pd.read_excel("Assets/20250617_cNPDB.xlsx")
 
 # Ensure numeric columns are numeric
 numeric_cols = ['Monoisotopic Mass', 'Length', 'GRAVY', '% Hydrophobic Residue', 'Instability Index Value', 'Isoelectric Point (pI)', 'Net Charge (pH 7.0)', 'Aliphatic Index', 'Boman Index']
