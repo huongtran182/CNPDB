3e65a6a932536eebf95c03656ad73b403d2cf07a
huongtran182 <92384719+huongtran182@users.noreply.github.com>
2025-06-22 20:26:33 -0500

    Update 1_NP_Database_Search.py

--- pages/1_NP_Database_Search.py
+++ pages/1_NP_Database_Search.py
@@ -284,7 +284,6 @@ def display_peptide_details(row: pd.Series):
          # Meta PDB file
         meta_pdb_file = f"Assets/3D Structure/3D Meta cNP{cNPDB_id}.pdb"
         if os.path.exists(meta_pdb_file):
-            st.markdown("<hr style='margin:20px 0;'>", unsafe_allow_html=True)
             st.markdown(
                 "<div style='text-align:center;font-weight:bold;color:#6a51a3;'>Meta-predicted 3D Structure</div>",
                 unsafe_allow_html=True
