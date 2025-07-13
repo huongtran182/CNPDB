d4dd0bd11bbaf650f7b40fe575d47952f896fa0d
lingjunli-research <90784754+lingjunli-research@users.noreply.github.com>
2025-07-13 10:24:36 -0500

    Update 1_NP_Database_Search.py

--- pages/1_NP_Database_Search.py
+++ pages/1_NP_Database_Search.py
@@ -140,7 +140,7 @@ def display_peptide_details(row: pd.Series):
         <table>
             <tr>
             <td style="background-color:#6a51a3; color:white; padding:4px 8px; line-height:1.2; border-radius: 10px 0 0 10px; ">cNPDB ID</td>
-            <td style="background-color:white; border:1px solid #6A0DAD; padding:4px 8px; line-height:1.2; border-radius: 0 10px 10px 0; ">{disp(row['cNPDB ID'])}</td>
+            <td style="background-color:white; border:1px solid #6A0DAD; padding:4px 8px; line-height:1.2; border-radius: 0 10px 10px 0; ">{disp(int(row["cNPDB ID"]))}</td>
             </tr>
             <tr>
             <td style="background-color:#6a51a3; color:white; padding:4px 8px; line-height:1.2; border-radius: 10px 0 0 10px; ">Family</td>
