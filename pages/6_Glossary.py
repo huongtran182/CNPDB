0cdb3c77ed7202ecbdf02769cce7ebb2f1e8dd79
lingjunli-research <90784754+lingjunli-research@users.noreply.github.com>
2025-07-29 11:19:02 -0500

    Update 6_Glossary.py

--- pages/6_Glossary.py
+++ pages/6_Glossary.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from sidebar import render_sidebar
 
+from session_tracker import track_session
+track_session()
+
 st.set_page_config(
     page_title="Glossary",
     layout="wide",
