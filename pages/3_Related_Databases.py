0504b6bf898fdde64ced5c8c5283a943ff6e9561
lingjunli-research <90784754+lingjunli-research@users.noreply.github.com>
2025-07-28 22:16:23 -0500

    Update 3_Related_Databases.py

--- pages/3_Related_Databases.py
+++ pages/3_Related_Databases.py
@@ -10,15 +10,17 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
+components.html(f"""
 <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-VWK5FWH61R"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
+  function gtag(){{dataLayer.push(arguments);}}
   gtag('js', new Date());
 
   gtag('config', 'G-VWK5FWH61R');
 </script>
+""", height=0)
 
 render_sidebar()
 
